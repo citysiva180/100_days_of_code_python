f7396575b0a0ba085e0752ab035fc0f0753f78dd
Sivarajan <citysiva180@users.noreply.github.com>
2024-08-02 07:48:05 +0530

    Practice Session  -  Hash table Class

--- PRACTICE_FOLDER/hashtable_in_python.py
+++ PRACTICE_FOLDER/hashtable_in_python.py
@@ -1,13 +1,18 @@
 class HashTable:
     def __init__(self, size=7):
-        # At first the data_map attribute will have an empty key value pair of size = 7
+       
         self.data_map = [None] * size
 
     def __hash(self, key):
         my_hash = 0
         for letter in key:
-            my_hash = (my_hash + ord(letter)*23) % len(self.data_map) # In this expression we understand that we are initializing a hashmap with 7 keys. By which 
-            # if we divi
+            my_hash = (my_hash + ord(letter)*23) % len(self.data_map) 
+
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
 
     def print_table(self):
         for index, val in enumerate(self.data_map):
@@ -16,3 +21,13 @@ class HashTable:
 
 my_hash_table = HashTable()
 my_hash_table.print_table()
+
+
+
+    # after iterating the string which is in key, we are getting the 
+    # ascii value of that letter and multiplying it by 23. 23 is a prime number. But why 23 right? 
+    # Then we are performing a modulo operation on the my_hash + ordinal value of letter multiplied by 23 
+    # and then getting the reminder which becomes the end length of the code 
+    
+     # At first the data_map attribute will have an empty key value pair of size = 7
+
---
+++ day_6_largest_binary_subarray.py
@@ -0,0 +1,2 @@
+# Find the largest subarray having an equal number of 0’s and 1’s
+# Given a binary array containing 0’s and 1’s, find the largest subarray with equal numbers of 0’s and 1’s
